f6e6f144aa118e10886ead94a445c1d561fa4f6d
Osama Makhlouph <30751973+Osama-Makhlouph@users.noreply.github.com>
2022-07-26 15:52:43 +0200

    Implemented api functionallities of subject-list, subject-detail , and course enrollment

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,11 +1,29 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.permissions import IsAuthenticated
 from ..models import Subject
+from ..models import Course
 from .serializers import SubjectSerializer
+
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
+
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+class CourseEnrollView(APIView):
+    authentication_classes = (BaseAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
